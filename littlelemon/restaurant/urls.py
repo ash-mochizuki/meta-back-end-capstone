b2322888adfa1becb84bf9a8009c6ea4f53e0a86
Ashton <38923493+ash-mochizuki@users.noreply.github.com>
2024-05-31 00:02:02 -0400

    Serializers and Rest stuff

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,15 @@
-from django.contrib import admin
-from django.urls import path
-from .views import sayHello
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import include, path
 from . import views
 
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
-    path('', sayHello, name='sayHello'),
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 ]
